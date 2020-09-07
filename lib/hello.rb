cc81fab00d9be10a20eae55b2e5bbc143fa721cc
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-07 00:53:58 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,15 +1,13 @@
-def hello_t(array)
-  if block_given?
-  i = 0 
-  while i < array.length 
-  yield(array[i])
-  i = i + 1 
+def my_each(array)# put argument(s) here
+  counter = 0 
+  while counter < array.length 
+  yield(array[counter])
+  counter = counter + 1 
 end 
-array
-else
-  puts "Hey! No block was given!"
-end
+array 
 end
 
 # call your method here!
+my_array =["kate", "john", "max"]
 
+my_each(my_array){|i| puts i}
